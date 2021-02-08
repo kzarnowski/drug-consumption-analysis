160f08abe30ac58c62af880d368e882987db2014
kzarnowski <45438132+kzarnowski@users.noreply.github.com>
2021-02-08 15:17:11 +0100

    Add files via upload

--- projekt1.r
+++ projekt1.r
@@ -1,9 +1,9 @@
 # DATA PREPARATION
 
-data <- read.delim("drug_consumption.data", header = FALSE, sep = ",", fill = TRUE)
+drugs <- read.delim("drug_consumption.data", header = FALSE, sep = ",", fill = TRUE)
   
 #str(data)
-colnames(data) <- c(
+colnames(drugs) <- c(
   "id", #1
   "age", #2
   "gender", #3
@@ -39,9 +39,9 @@ colnames(data) <- c(
 ) 
 
 # quantitive data mapping
-data[ ,c(2:11)] <- lapply(data[ ,c(2:11)], as.character)
+drugs[ ,c(2:11)] <- lapply(drugs[ ,c(2:11)], as.character)
 
-#drugs
+#usage
 usage <- c(
   "Never Used",
   "Used over a Decade Ago", 
@@ -54,9 +54,9 @@ usage <- c(
 names(usage) <- c("CL0", "CL1", "CL2", "CL3", "CL4", "CL5", "CL6")
 names
 for (i in 14:32) {
-  tmp <- usage[data[ ,i]]
-  data[ ,i] <- as.factor(tmp)
-  levels(data[ ,i]) <- usage
+  tmp <- usage[drugs[ ,i]]
+  drugs[ ,i] <- as.factor(tmp)
+  levels(drugs[ ,i]) <- usage
 }
 
 # age
@@ -69,15 +69,15 @@ names(ageLevels) <- c(
   1.82213,  # 55-64
   2.59171   # 65+
 )  
-tmp <- ageLevels[data$age]
-data$age <- as.factor(tmp)
-levels(data$age) <- ageLevels
+tmp <- ageLevels[drugs$age]
+drugs$age <- as.factor(tmp)
+levels(drugs$age) <- ageLevels
 
 # gender
 genderLevels <- c("M", "F")
 names(genderLevels) <- c(-0.48246, 0.48246)
-tmp <- genderLevels[data$gender]
-data$gender <- as.factor(tmp)
+tmp <- genderLevels[drugs$gender]
+drugs$gender <- as.factor(tmp)
 
 #education
 educationLevels <- c(
@@ -85,25 +85,26 @@ educationLevels <- c(
   "Left school at 16 years",
   "Left school at 17 years",
   "Left school at 18 years",
-  "Some college or university, no certificate or degree",
-  "Professional certificate/ diploma",
+  "Some college, no degree",
+  "Professional certificate",
   "University degree",
-  "Masters degree","Doctorate degree"
+  "Masters degree",
+  "Doctorate degree"
 )
 names(educationLevels) <- c(
   -2.43591, # Left school before 16 years
   -1.73790, # Left school at 16 years
   -1.43719, # Left school at 17 years
   -1.22751, # Left school at 18 years
-  -0.61113, # Some college or university, no certificate or degree
-  -0.05921, # Professional certificate/ diploma
+  -0.61113, # Some college, no degree
+  -0.05921, # Professional certificate
   0.45468,  # University degree
   1.16365,  # Masters degree  
   1.98437   # Doctorate degree
 )
-tmp <- educationLevels[data$education]
-data$education <- as.factor(tmp)
-levels(data$education) <- educationLevels
+tmp <- educationLevels[drugs$education]
+drugs$education <- as.factor(tmp)
+levels(drugs$education) <- educationLevels
 
 #country
 countryLevels <- c("Australia", "Canada", "New Zealand", "Other",
@@ -117,8 +118,8 @@ names(countryLevels) <- c(
   0.96082,  # UK
   -0.57009  # USA
 )
-tmp <- countryLevels[data$country]
-data$country <- as.factor(tmp)
+tmp <- countryLevels[drugs$country]
+drugs$country <- as.factor(tmp)
 
 #ethnicity
 ethnicityLevels <- c("Asian", "Black", "Mixed-Black/Asian", "Mixed-White/Asian",
@@ -132,8 +133,8 @@ names(ethnicityLevels) <- c(
   0.11440,  # Other
   -0.31685  # White
 )
-tmp <- ethnicityLevels[data$ethnicity]
-data$ethnicity <- as.factor(tmp)
+tmp <- ethnicityLevels[drugs$ethnicity]
+drugs$ethnicity <- as.factor(tmp)
 
 
 # WORKING WITH TIDYVERSE
@@ -146,126 +147,103 @@ ascoreLevels <- c(12,16,18,23:60)
 cscoreLevels <- c(17,19:57,59)
 
 #nscores
-scores <- data %>%
+scores <- drugs %>%
   distinct(nscore) %>%
   arrange(nscore) %>%
   pull(nscore)
 
 names(nscoreLevels) <- scores
-tmp <- nscoreLevels[data$nscore]
-data$nscore <- as.factor(tmp)
+tmp <- nscoreLevels[drugs$nscore]
+drugs$nscore <- as.factor(tmp)
 
 #escores
-scores <- data %>%
+scores <- drugs %>%
   distinct(escore) %>%
   arrange(escore) %>%
   pull(escore)
 
 names(escoreLevels) <- scores
-tmp <- escoreLevels[data$escore]
-data$escore <- as.factor(tmp)
+tmp <- escoreLevels[drugs$escore]
+drugs$escore <- as.factor(tmp)
 
 #oscores
-scores <- data %>%
+scores <- drugs %>%
   distinct(oscore) %>%
   arrange(oscore) %>%
   pull(oscore)
 
 names(oscoreLevels) <- scores
-tmp <- oscoreLevels[data$oscore]
-data$oscore <- as.factor(tmp)
+tmp <- oscoreLevels[drugs$oscore]
+drugs$oscore <- as.factor(tmp)
 
 #ascores
-scores <- data %>%
+scores <- drugs %>%
   distinct(ascore) %>%
   arrange(ascore) %>%
   pull(ascore)
 
 names(ascoreLevels) <- scores
-tmp <- ascoreLevels[data$ascore]
-data$ascore <- as.factor(tmp)
+tmp <- ascoreLevels[drugs$ascore]
+drugs$ascore <- as.factor(tmp)
 
 #cscores
-scores <- data %>%
+scores <- drugs %>%
   distinct(cscore) %>%
   arrange(cscore) %>%
   pull(cscore)
 
 names(cscoreLevels) <- scores
-tmp <- cscoreLevels[data$cscore]
-data$cscore <- as.factor(tmp)
+tmp <- cscoreLevels[drugs$cscore]
+drugs$cscore <- as.factor(tmp)
 
-#nscore
-# nscoreLevels <- c(12:60)
-# names(nscoreLevels) <- c(
-#   -3.46436, -3.15735, -2.75696, -2.52197, -2.42317, -2.34360, -2.21844, -2.05048, 
-#   -1.86962, -1.69163, -1.55078, -1.43907, -1.32828, -1.19430, -1.05308, -0.92104, 
-#   -0.79151, -0.67825, -0.58016, -0.46725, -0.34799, -0.24649, -0.14882, -0.05188, 
-#    0.04257,  0.13606,  0.22393,  0.31287,  0.41667,  0.52135,  0.62967,  0.73545,
-#    0.82562,  0.91093,  1.02119,  1.13281,  1.23461,  1.37297,  1.49158,  1.60383, 
-#    1.72012,  1.83990,  1.98437,  2.12700,  2.28554,  2.46262,  2.61139,  2.82196,
-#    3.27393
-# )
-# tmp <- nscoreLevels[data$nscore]
-# data$nscore <- as.factor(tmp)
+drugs[ ,c(7:11)] <- lapply(drugs[ ,c(7:11)], as.numeric)
 
-#escore
-# escoreLevels <- c(16,18:59)
-# names(escoreLevels) <- c(
-#   -3.27393, -3.11111, -3.00537, -2.72827, -2.53830, -2.44904, -2.32338, -2.21069, 
-#   -2.11437, -2.03972, -1.92173, -1.76250, -1.63340, -1.50796, -1.37639, -1.23177,
-#   -1.09207, -0.94779, -0.80615, -0.69509, -0.57545, -0.43999, -0.30033, -0.15487,
-#    0.00332,  0.16767,  0.32197,  0.47617,  0.63779,  0.80523,  0.96248,  1.11406,
-#    1.28610,  1.45421,  1.58487,  1.74091,  1.93886,  2.12700,  2.32338,  2.57309,
-#    2.85950,  3.00537,  3.27393
-# )
-# tmp <- escoreLevels[data$escore]
-# data$escore <- as.factor(tmp)
-
-#oscore
-# oscoreLevels <- c(24,26,28:60)
-# names(oscoreLevels) <- c(
-#   -3.27393, -3.11111, -2.85950, -2.63199, -2.39883, -2.21069, -2.09015, -1.97495,
-#   -1.82919, -1.68062, -1.55521, -1.42424, -1.27553, -1.11902, -0.97631, -0.84732,
-#   -0.71727, -0.58331, -0.45174, -0.31776, -0.17779, -0.01928,  0.14143,  0.29338,
-#    0.44585,  0.58331,  0.72330,  0.88309,  1.06238,  1.24033,  1.43533,  1.65653,
-#    1.88511,  2.15324,  2.44904,  2.90161
-# )
-
-# ascoreLevels <- c(12,16,18,23:60)
-# names(ascoreLevels) <- c(
-#   -3.46436, -3.15735, -3.00537, -2.90161, -2.78793, -2.70172, -2.53830, -2.35413, 
-#   -2.21844, -2.07848, -1.92595, -1.77200, -1.62090, -1.47955, -1.34289, -1.21213, 
-#   -1.07533, -0.91699, -0.76096, -0.60633, -0.45321, -0.30172, -0.15487, -0.01729,
-#    0.13136,  0.28783,  0.43852,  0.59042,  0.76096,  0.94156,  1.11406,  1.28610,
-#    1.45039,  1.61108,  1.81866,  2.03972,  2.23427,  2.46262,  2.75696,  3.15735,
-#    3.46436
-# )
-
-# cscoreLevels <- c(17,19:57,59)
-# names(cscoreLevels) <- c(
-#   -3.46436, -3.15735, -2.90161, -2.72827, -2.57309, -2.42317, -2.30408, -2.18109,
-#   -2.04506, -1.92173, -1.78169, -1.64101, -1.51840, -1.38502, -1.25773, -1.13788,
-#   -1.01450, -0.89891, -0.78155, -0.65253, -0.52745, -0.40581, -0.27607, -0.14277,
-#   -0.00665,  0.12331,  0.25953,  0.41594,  0.58489,  0.75830,  0.93949,  1.13407,
-#    1.30612,  1.46191,  1.63088,  1.81175,  2.04506,  2.33337,  2.63199,  3.00537,
-#    3.46436
-# )
-
-
-if (sum(is.na(data)) > 0) {
-  drop_na(data)
+if (sum(is.na(drugs)) > 0) {
+  drop_na(drugs)
 } else {
   print("No missing values")
 }
 
-summary(data) # for each category
+summary(drugs) # for each category
 
-write.csv(data, "output.csv", row.names = FALSE)
-saveRDS(data, "output.Rda")
+write.csv(drugs, "output.csv", row.names = FALSE)
+saveRDS(drugs, "output.Rda")
+
+#LATTICE
+
+library(lattice)
+library(latticeExtra)
+
+histogram(~amphetamine|factor(gender),
+          data = drugs,
+          type = "count",
+          auto.key = FALSE)
+
+?histogram
 
 
+#GGPLOT
+library(ggplot2)
+# drugs %>%
+#   group_by(cocaine) %>%
+#   summarise(avg = mean(nscore))
+
+ggplot(data = drugs, aes(x = cannabis, fill = age)) +
+  geom_bar() +
+  theme_bw() + 
+  facet_wrap(~ gender)
+
+illegal <- c(15,19,21:25,27:29,31)
+illegal_count <- rowSums(drugs[ ,illegal] != "Never Used", na.rm = TRUE)
+
+drugs <- mutate(drugs, illegal_active = illegal_count)
+ggplot(data = drugs, aes(x = illegal_active, y = education)) + 
+  geom_count(aes(size = ..n..), color='royalblue3', shape = 15) +
+  ggtitle("Correlation between illegal drugs usage and education") +
+  labs(x = "Number of illegal drugs ever used", y = "Education level") +
+  scale_size("Group", range = c(1,8)) 
 
 
+?geom_count
 
 
